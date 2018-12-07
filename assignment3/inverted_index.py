86e2e8feb2da3a6c959ea8ca50330e0c37a6fd6c
andylu <andy0128.lu@gmail.com>
2018-12-07 17:36:58 +1100

    update

---
+++ assignment3/inverted_index.py
@@ -0,0 +1,33 @@
+import MapReduce
+import sys
+
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      mr.emit_intermediate(w.encode('utf-8').lower(), key.encode('utf-8'))
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of documents
+    """
+    output = []
+    output.append(key)
+    output.append(list(set(list_of_values)))
+    """
+    mr.emit((key, list(set(list_of_values))))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
